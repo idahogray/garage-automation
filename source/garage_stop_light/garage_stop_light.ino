f67b664f03497f713352912a724e652da9802d78
Keith Gray <idahogray@gmail.com>
2013-10-12 20:49:32 -0500

    Added stop LED

--- source/garage_stop_light/garage_stop_light.ino
+++ source/garage_stop_light/garage_stop_light.ino
@@ -2,19 +2,26 @@
 
 #define PING_PIN 12
 #define MAX_DISTANCE 200
+#define LED_PIN 13
 
 NewPing sonar(PING_PIN, PING_PIN, MAX_DISTANCE);
 
 void setup()
 {
   Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-  delay(50);
+  delay(500);
   unsigned int uS = sonar.ping();
+  unsigned int distance_cm = uS / US_ROUNDTRIP_CM;
   Serial.print("Ping: ");
-  Serial.print(uS / US_ROUNDTRIP_CM);
+  Serial.print(distance_cm);
   Serial.println("cm");
+  if (distance_cm < 50)
+    digitalWrite(LED_PIN, HIGH);
+  else
+    digitalWrite(LED_PIN, LOW);
 }
