c4e7ea4533bac22b3adaece18e5662a148b38571
Keith Gray <idahogray@gmail.com>
2013-10-19 20:42:34 -0500

    Added temperature in *F to modbus map, in addition to *C

--- source/garage_stop_light/garage_stop_light.ino
+++ source/garage_stop_light/garage_stop_light.ino
@@ -23,7 +23,8 @@ DHT dht(DHT_PIN, DHT_TYPE);
 enum
 {
   HUMIDITY,
-  TEMPERATURE,
+  TEMPERATURE_C,
+  TEMPERATURE_F,
   BAY_1_DISTANCE_CM,
   BAY_2_DISTANCE_CM,
   BAY_1_LED_STATE,
@@ -50,9 +51,9 @@ void loop()
 {
   delay(1000);
   unsigned int bay1_uS = bay1.ping();
-  unsigned int bay1_distance_cm = bay1_uS / US_ROUNDTRIP_CM;
+  unsigned int bay1_distance_cm = 10; //bay1_uS / US_ROUNDTRIP_CM;
   unsigned int bay2_uS = bay2.ping();
-  unsigned int bay2_distance_cm = bay2_uS / US_ROUNDTRIP_CM;
+  unsigned int bay2_distance_cm = 20; //bay2_uS / US_ROUNDTRIP_CM;
   
   Serial.print("Bay 1 Ping: ");
   Serial.print(bay1_distance_cm);
@@ -85,12 +86,17 @@ void loop()
     Serial.print(" %\t");
     Serial.print("Temperature: "); 
     Serial.print(temperature);
-    Serial.println(" *C");
+    Serial.print(" *C");
+    Serial.print("\t");
+    Serial.print("Temperature: "); 
+    Serial.print(temperature * 1.8 + 32);
+    Serial.println(" *F");
   }
     
   holding_registers[TOTAL_ERRORS] = modbus_update(holding_registers);
   holding_registers[HUMIDITY] = humidity * 100;
-  holding_registers[TEMPERATURE] = temperature * 100;
+  holding_registers[TEMPERATURE_C] = temperature * 100;
+  holding_registers[TEMPERATURE_F] = (temperature * 1.8 + 32) * 100;
   holding_registers[BAY_1_DISTANCE_CM] = bay1_distance_cm;
   holding_registers[BAY_2_DISTANCE_CM] = bay2_distance_cm;
   holding_registers[BAY_1_LED_STATE] = digitalRead(BAY_1_LED_PIN);
@@ -98,10 +104,12 @@ void loop()
   
   Serial.print("Total Errors: ");
   Serial.println(holding_registers[TOTAL_ERRORS]);
-  Serial.print("Humidity: ");
+  Serial.print("Humidity (% * 100): ");
   Serial.println(holding_registers[HUMIDITY]);
-  Serial.print("Temperature (*C): ");
-  Serial.println(holding_registers[TEMPERATURE]);
+  Serial.print("Temperature (*C * 100): ");
+  Serial.println(holding_registers[TEMPERATURE_C]);
+  Serial.print("Temperature (*F * 100): ");
+  Serial.println(holding_registers[TEMPERATURE_F]);
   Serial.print("Bay 1 Distance (cm): ");
   Serial.println(holding_registers[BAY_1_DISTANCE_CM]);
   Serial.print("Bay 2 Distance (cm): ");
